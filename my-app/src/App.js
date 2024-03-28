b06c2cbb351b7e3e877933f39807a0aed1eedff3
Tran Minh Duc <dinhhoangducdbp2004@gmail.com>
2024-03-28 14:58:39 +0700

    Day01

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
-import logo from "./logo.svg";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
@@ -17,7 +17,6 @@ function App() {
         >
           Learn React
         </a>
-        <button>Button</button>
       </header>
     </div>
   );
