0ab9d07e20c028ffa65659b188034e6016f324bb
Tran Minh Duc <dinhhoangducdbp2004@gmail.com>
2024-03-28 15:12:38 +0700

    Day01

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import TmD from "./components/TmD";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <TmD email="Dinhhoangducdbp2004@gmail.com" contact="0855312279" />
     </div>
   );
 }
